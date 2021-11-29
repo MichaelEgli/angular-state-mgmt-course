20f12b86dc0e3bc801b4ce97202ea225de06c39e
MichaelEgli <michaelegli1879@gmail.com>
2021-11-29 11:43:12 +0100

    ex selectors written

--- exercise-1-selectors/projects/todo-app/src/app/features/todo/state/todo.selectors.ts
+++ exercise-1-selectors/projects/todo-app/src/app/features/todo/state/todo.selectors.ts
@@ -9,10 +9,19 @@ export const selectTodoFeature = createFeatureSelector<State>(todoFeatureKey);
 export const selectTodoFilter = createSelector(selectTodoFeature, state => state.todoFilter);
 
 // TODO 3: create "selectTodos" selector to select todo items as an array (please return only items which are valid based on active filter)
-export const selectTodos = undefined;
+export const selectTodos = createSelector(selectTodoFeature, selectTodoFilter, (todo, filter) => 
+Object.values(todo.items).filter(item => {
+    if (filter === 'ALL') {
+        return true;
+    } else if (filter === 'DONE') {
+        return item.done;
+    } else if (filter === 'ACTIVE') {
+        return !item.done;
+    }
+}));
 
 // TODO 4: create "selectTodosCount" selector to select todo items count (there at least two ways to do this, eg: array vs object)
-export const selectTodosCount = undefined;
+export const selectTodosCount = createSelector(selectTodos, todos => todos.length);
 
 // TODO 5: create "selectEditedTodo" selector to select todo item that is being edited (state has a "editedTodoId" property)
-export const selectEditedTodo = undefined;
+export const selectEditedTodo = createSelector(selectTodoFeature, todo => todo.items[todo.editedTodoId]);
